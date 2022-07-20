18660ac2ac48b7dc21df4a2c2de8d5bab2fb329f
shlocko <mcdude22@gmail.com>
2022-07-19 18:15:33 -0700

    Added basic system support

--- Engine.ts
+++ Engine.ts
@@ -4,23 +4,40 @@ type system = (game: Game) => void
 
 export class Game  {
     systems: Array<system>;
+    lastRenderTime: number;
     
     constructor() {
         this.systems = [];
+        this.lastRenderTime = 0;
     }
-
+    
     createDefaultRenderSystem(canvasElement: HTMLCanvasElement, width: number, height: number) {
         const canvas = new Canvas(canvasElement, width, height);
         this.systems.push((game: Game) => {
             canvas.clear("blue");
+            console.log("render");
         });
     }
-
-
-   init() {
+    
+    addSystem(system: system){
+       this.systems.push(system); 
+    }
+    
+    private loop(timestamp){
+        let deltaTime: number = timestamp - this.lastRenderTime;
+        
         this.systems.forEach(fn => {
             fn(this);
         });
+        
+       this.lastRenderTime = timestamp;
+       requestAnimationFrame((timestamp)=>this.loop(timestamp/1000));
+       
+    }
+    
+    
+   init() {
+        requestAnimationFrame((timestamp)=>this.loop(timestamp/1000));
     }
 }
 
