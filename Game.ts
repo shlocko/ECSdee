c0a3d7218638eceb8a47c1b9bdd3c450c32409a3
shlocko <mcdude22@gmail.com>
2022-07-21 11:32:33 -0700

    Fixed engine code being in wrong file, Engine.ts now exports all engine classes as intended

---
+++ Game.ts
@@ -0,0 +1,42 @@
+import {Canvas} from "./Engine";
+
+type system = (game: Game) => void
+
+export class Game  {
+    systems: Array<system>;
+    lastRenderTime: number;
+
+    constructor() {
+        this.systems = [];
+        this.lastRenderTime = 0;
+    }
+
+    createDefaultRenderSystem(canvasElement: HTMLCanvasElement, width: number, height: number) {
+        const canvas = new Canvas(canvasElement, width, height);
+        this.systems.push((game: Game) => {
+            canvas.clear("blue");
+            console.log("render");
+        });
+    }
+
+    addSystem(system: system){
+        this.systems.push(system);
+    }
+
+    private loop(timestamp){
+        let deltaTime: number = timestamp - this.lastRenderTime;
+
+        this.systems.forEach(fn => {
+            fn(this);
+        });
+
+        this.lastRenderTime = timestamp;
+        requestAnimationFrame((timestamp)=>this.loop(timestamp/1000));
+
+    }
+
+
+    init() {
+        requestAnimationFrame((timestamp)=>this.loop(timestamp/1000));
+    }
+}
